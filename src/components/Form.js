8b0defe53b5b043367e9a34d90c7ed79a7c710c7
yilfri <yilfri97@hotmail.com>
2021-06-05 14:30:02 -0400

    Form component started

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,10 +2,44 @@ import React from 'react';
 
 const Form = () => {
 	return (
-		<div>
-			<>
-				<h1>Formulario</h1>
-			</>
+		<div className="bg-info">
+			<div className="container">
+				<div className="row">
+					<form className="col card text-white bg-transparent mb-5 pt-5 pb-2">
+						<fieldset>
+							<legend className="text-center">Buscador Letras Canciones</legend>
+
+							<div className="row">
+								<div className="col-md-6">
+									<div className="form-group">
+										<label>Artista</label>
+										<input
+											type="text"
+											name="artist"
+											placeholder="Nombre Artista"
+											className="form-control"
+										/>
+									</div>
+								</div>
+								<div className="col-md-6">
+									<div className="form-group">
+										<label>Artista</label>
+										<input
+											type="text"
+											name="song"
+											placeholder="Nombre Canción"
+											className="form-control"
+										/>
+									</div>
+								</div>
+							</div>
+							<button type="submit" className="btn btn-primary float-right">
+								Buscar
+							</button>
+						</fieldset>
+					</form>
+				</div>
+			</div>
 		</div>
 	);
 };
